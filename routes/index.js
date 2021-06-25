277409f83ead9954b351b72da3e86ae441b4e7b2
Joseph Mutua <mutuaj793@gmail.com>
2021-06-25 16:13:12 +0300

    Configure multer to upload files to system folders

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+
 const multer = require("multer");
+const upload = multer({ dest: "public/images/uploads" });
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/formsub", (req, res, next) => {
-  res.json(req.body);
+router.post("/formsub", upload.single("meme"), (req, res, next) => {
+  console.log(req.file);
+  const newPath = `public/images/uploads/${req.file.originalname}`;
+  fs.rename(req.file.path, newPath, () => {
+    res.json("File uploaded");
+  });
 });
 
 module.exports = router;
