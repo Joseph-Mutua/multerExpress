3ff4b00028fb6036d27055143cffe38f86e98571
Joseph Mutua <mutuaj793@gmail.com>
2021-06-25 15:59:29 +0300

    Add multer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const multer = require("multer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/formsub", (req, res, next) => {
+  res.json(req.body);
 });
 
 module.exports = router;
